f7b128c9a031f5232724d0bd22ef392db618f18e
MartinMZen <mmelekus@msn.com>
2023-06-07 20:12:38 -0500

    Created ADO.NET Console App

---
+++ Chapter02/Northwind.Console.SqlClient/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnectionStringBuilder builder = new();
+builder.InitialCatalog = "Northwind";
+builder.MultipleActiveResultSets = true;
+builder.Encrypt = true;
+builder.TrustServerCertificate = true;
+builder.ConnectTimeout = 10;
+
+WriteLine("Connect to:");
+WriteLine("  1 - SQL Server on local machine");
+WriteLine("  2 - Azure SQL Database");
+WriteLine("  3 - Azure SQL Edge");
+WriteLine();
+Write("Press a key: ");
+ConsoleKey key = ReadKey().Key;
+WriteLine(); WriteLine();
+
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    builder.DataSource = "."; // Local SQL Server
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    builder.DataSource = ".";
+    // builder.DataSource = "tcp:apps-services-net7.database.windows.net,1433"; // Azure SQL Database
+}
+else if (key is ConsoleKey.D3 or ConsoleKey.NumPad3)
+{
+    builder.DataSource = ".";
+    // builder.DataSource = "tcp:127.0.0.1,1433"; // Azure SQL Edge
+}
+else
+{
+    WriteLine("No data source selected.");
+    return;
+}
+
+WriteLine("Authenticate using:");
+WriteLine("  1 - Windows Integrated Security");
+WriteLine("  2 - SQL Login, for example, sa");
+WriteLine();
+Write("Press a key: ");
+key = ReadKey().Key;
+WriteLine(); WriteLine();
+
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    builder.IntegratedSecurity = true;
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    builder.UserID = "sa";
+    Write("Enter you SQL Server password: ");
+    string? password = ReadLine();
+    if (string.IsNullOrWhiteSpace(password))
+    {
+        WriteLine("Password cannot be empty or null.");
+        return;
+    }
+    builder.Password = password;
+    builder.PersistSecurityInfo = false;
+}
+else
+{
+    WriteLine("No authentication selected.");
+    return;
+}
+
+SqlConnection connection = new(builder.ConnectionString);
+WriteLine(connection.ConnectionString);
+WriteLine();
+connection.StateChange += Connection_StateChange;
+connection.InfoMessage += Connection_InfoMessage;
+
+try
+{
+    WriteLine("Opening connection. Please wait upto {0} seconds...", builder.ConnectTimeout);
+    WriteLine();
+    connection.Open();
+    WriteLine($"SQL Server version: {connection.ServerVersion}");
+    connection.StatisticsEnabled = true;
+}
+catch (SqlException ex)
+{
+    WriteLine($"SQL exception: {ex.Message}");
+}
+finally
+{
+    connection.Close();
+}
