714fcfd6490d085c50ed5da11cf9a7f253abf873
MartinMZen <mmelekus@msn.com>
2023-06-11 12:34:31 -0500

    EF Core Unit Tests

---
+++ Chapter02/Northwind.Common.DataContext.SqlServer/SetLastRefreshedInterceptor.cs
@@ -0,0 +1,16 @@
+// IMaterializationInterceptor, MaterializationInterceptionData
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Packt.Shared;
+
+public class SetLastRefreshedInterceptor : IMaterializationInterceptor
+{
+    public object InitializedInstance(MaterializationInterceptionData materializationData, object entity)
+    {
+        if (entity is IHasLastRefreshed entityWithLastRefreshed)
+        {
+            entityWithLastRefreshed.LastRefreshed = DateTimeOffset.UtcNow;
+        }
+        return entity;
+    }
+}
---
+++ Chapter02/Northwind.Common.EntityModels.SqlServer/IHasLastRefreshed.cs
@@ -0,0 +1,6 @@
+namespace Packt.Shared;
+
+public interface IHasLastRefreshed
+{
+    DateTimeOffset LastRefreshed { get; set; }
+}
