2895a13abfae432b4f5a152222ad1f1ae47e710a
MartinMZen <mmelekus@msn.com>
2023-06-11 07:44:02 -0500

    EFCore project

--- Chapter02/Northwind.Console.SqlClient/Program.cs
+++ Chapter02/Northwind.Console.SqlClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 SqlConnectionStringBuilder builder = new();
 builder.InitialCatalog = "Northwind";
@@ -77,7 +78,7 @@ try
 {
     WriteLine("Opening connection. Please wait upto {0} seconds...", builder.ConnectTimeout);
     WriteLine();
-    connection.Open();
+    await connection.OpenAsync();
     WriteLine($"SQL Server version: {connection.ServerVersion}");
     connection.StatisticsEnabled = true;
 }
@@ -85,7 +86,71 @@ catch (SqlException ex)
 {
     WriteLine($"SQL exception: {ex.Message}");
 }
-finally
+
+Write("Enter a unit price: ");
+string? priceText = ReadLine();
+if(!decimal.TryParse(priceText, out decimal price))
 {
-    connection.Close();
+    WriteLine("You must enter a valid unit price.");
+    return;
 }
+
+SqlCommand cmd = connection.CreateCommand();
+WriteLine("Execute command using:");
+WriteLine("  1 - Text");
+WriteLine("  2 - Stored Procedure");
+WriteLine();
+Write("Press a key: ");
+key = ReadKey().Key;
+WriteLine(); WriteLine();
+
+SqlParameter p1, p2 = new(), p3 = new();
+if (key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+{
+    cmd.CommandType = CommandType.Text;
+    cmd.CommandText = "SELECT ProductId, ProductName, UnitPrice FROM Products"
+        + " WHERE UnitPrice > @price";
+    cmd.Parameters.AddWithValue("price", price);
+}
+else if (key is ConsoleKey.D2 or ConsoleKey.NumPad2)
+{
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.CommandText = "GetExpensiveProducts";
+    p1 = new()
+    {
+        ParameterName = "price",
+        SqlDbType = SqlDbType.Money,
+        SqlValue = price
+    };
+    p2 = new()
+    {
+        Direction = ParameterDirection.Output,
+        ParameterName = "count",
+        SqlDbType = SqlDbType.Int
+    };
+    p3 = new()
+    {
+        Direction = ParameterDirection.ReturnValue,
+        ParameterName = "rv",
+        SqlDbType = SqlDbType.Int
+    };
+    cmd.Parameters.Add(p1);
+    cmd.Parameters.Add(p2);
+    cmd.Parameters.Add(p3);
+}
+SqlDataReader r = await cmd.ExecuteReaderAsync();
+WriteLine("----------------------------------------------------------");
+WriteLine("| {0,5} | {1,-35} | {2,8} |", "Id", "Name", "Price");
+WriteLine("----------------------------------------------------------");
+while (await r.ReadAsync())
+{
+    WriteLine("| {0,5} | {1,-35} | {2,8:C} |",
+        await r.GetFieldValueAsync<int>("ProductId"),
+        r.GetFieldValueAsync<string>("ProductName"),
+        r.GetFieldValueAsync<decimal>("UnitPrice"));
+}
+WriteLine("----------------------------------------------------------");
+await r.CloseAsync();
+WriteLine($"Output count: {p2.Value}");
+WriteLine($"Return value: {p3.Value}");
+await connection.CloseAsync();
