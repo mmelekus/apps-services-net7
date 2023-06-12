0adc9a6f81e47177323a4231cf4347a865efc661
MartinMZen <mmelekus@msn.com>
2023-06-11 23:21:03 -0500

    Performance test of string vs stringbuilder

---
+++ Chapter04/PerformanceMonitoring/MonitoringApp/Program.Helpers.cs
@@ -0,0 +1,14 @@
+namespace MonitoringApp;
+
+partial class Program
+{
+    static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previousColor;
+    }
+}
--- Chapter04/PerformanceMonitoring/MonitoringApp/Program.cs
+++ Chapter04/PerformanceMonitoring/MonitoringApp/Program.cs
@@ -1,9 +1,39 @@
-﻿using MonitoringLib;
+﻿using System.Text;
+using MonitoringLib;
 
-WriteLine("Processing. Please wait...");
-Recorder.Start();
-// simulate a process that requires some memory resources...
-int[] largeArrayOfInts = Enumerable.Range(start: 1, count: 10_000).ToArray();
-// ...and takes some time to complete
-Thread.Sleep(new Random().Next(5, 10) * 1000);
-Recorder.Stop();
+// WriteLine("Processing. Please wait...");
+// Recorder.Start();
+// // simulate a process that requires some memory resources...
+// int[] largeArrayOfInts = Enumerable.Range(start: 1, count: 10_000).ToArray();
+// // ...and takes some time to complete
+// Thread.Sleep(new Random().Next(5, 10) * 1000);
+// Recorder.Stop();
+namespace MonitoringApp;
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = Enumerable.Range(start: 1, count: 50_000).ToArray();
+        SectionTitle("Using StringBuilder");
+
+        Recorder.Start();
+        StringBuilder builder = new();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            builder.Append(numbers[i]);
+            builder.Append(", ");
+        }
+        Recorder.Stop();
+
+        WriteLine();
+        SectionTitle("Using string with +");
+        Recorder.Start();
+        string s = string.Empty;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            s += numbers[i] + ", ";
+        }
+        Recorder.Stop();
+    }
+}
